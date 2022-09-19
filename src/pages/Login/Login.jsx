bf55f660c00c8c1e0194325bac2b72488669bd03
Joel Acquah <joelaj06@gmail.com>
2022-09-19 21:34:49 +0000

    user form validation implemented

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,6 +8,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import NotificationService from "../../services/notification_service/notification_service";
 
 const Login = () => {
   const user = <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>;
@@ -15,40 +16,109 @@ const Login = () => {
   const eye = <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>;
   const eyeSlash = <FontAwesomeIcon icon={faEyeSlash}></FontAwesomeIcon>;
 
+  const notificationService = new NotificationService();
+
+  const formData = {
+    email : '',
+    password : ''
+  }
   
+  // states
   const [isVisible, setVisibility] = useState(false);
+  const [isEnabled, setEnableButton] = useState(true);
+
+
   const showPassword = () => {
-    const password = document.getElementById('password');
-    if(!isVisible) {
-      password.setAttribute('type', 'text');
+    const password = document.getElementById("password");
+    if (!isVisible) {
+      password.setAttribute("type", "text");
+    } else {
+      password.setAttribute("type", "password");
+    }
+  };
+
+  const onEmailInputChange = (event) => { 
+    formData.email = event.target.value;
+  }
+
+  const onPasswordInputChange = (event) => {
+     formData.password = event.target.value; 
+  }
+
+  const validateEmail = (email) => {
+    console.log(typeof email)
+    var regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(email && String(email).toLowerCase().match(regExp)){
+      return true;
     }else{
-      password.setAttribute('type', 'password');
+       notificationService.showError('Fields Cannot Be Empty');
+       return false;
     }
   }
+
+  const validatePassword = (password) => {
+    console.log(typeof password)
+    if(password.length < 6){
+      notificationService.showError('Invalid Password');
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  const onFormSubmit = (e) => {
+      e.preventDefault();   
+    //  console.log(formData)
+      if(validateEmail(formData.email) && validatePassword(formData.password)){
+        notificationService.showSuccess('Valid Credentials')
+      }else{
+        return;
+      }
+  };
+
   return (
     <div className="main-container">
-      <div className="login-container">
-        <form action="">
-          <h3>Login</h3>
-          <div className="input-wrapper email">
-            <span className="icon">{user}</span>
-            <input type="email" name="email" id="email" placeholder="Email" />
-          </div>
-          <div className="input-wrapper password">
-            <span className="icon">{lock}</span>
-          <div className="password-container">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-            />
-            <span className="view-password" onClick={ () => { setVisibility(!isVisible)
-            showPassword()}}>{isVisible ? eyeSlash : eye}</span>
-          </div>
-          </div>
-          <input type="submit" value="Sign in" />
-        </form>
+      <div className="background-dark-overlay">
+        <div className="login-container">
+          <form onSubmit={onFormSubmit}>
+            <h3>Login</h3>
+            <div className="input-fields">
+              <div className="input-wrapper email">
+                <span className="icon">{user}</span>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  onChange={onEmailInputChange}
+                />
+              </div>
+              <div className="input-wrapper password">
+                <span className="icon">{lock}</span>
+                <div className="password-container">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={onPasswordInputChange}
+                  />
+                  <span
+                    className="view-password"
+                    onClick={() => {
+                      setVisibility(!isVisible);
+                      showPassword();
+                    }}
+                  >
+                    {isVisible ? eyeSlash : eye}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <input type="submit" value="Sign in" />
+          </form>
+        </div>
       </div>
     </div>
   );
---
+++ src/services/notification_service/notification_service.js
@@ -0,0 +1,38 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
+
+class NotificationService {
+  #toastConfig = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme : 'colored',
+    
+  };
+
+  showSuccess = (message) => {
+    console.log(message);
+    return toast.success(message, this.#toastConfig);
+  };
+
+  showError = (message) => {
+    console.log("showing error");
+    return toast.error(message, this.#toastConfig);
+  };
+
+  showInfo = (message) => {
+    toast.info(message, this.#toastConfig);
+  };
+
+  showWarning = (message) => {
+    toast.warning(message, this.#toastConfig);
+  };
+}
+
+export default NotificationService;
