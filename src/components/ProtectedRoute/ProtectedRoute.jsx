ebd70b30f82da1dfa2482a2fad51b4a489e4fdec
Joel Acquah <jacquah@appsolinfosystems.com>
2024-02-21 18:21:38 +0000

    Material UI migration stage 2

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { Outlet, useNavigate} from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import AuthService from "../../services/authentication_services/auth_service";
 
 const ProtectedRoute = () => {
   const authService = new AuthService();
   let navigate = useNavigate();
-  return authService.isAuthenticated ? <Outlet/> : navigate('/'); 
-    
+  return authService.isAuthenticated ? <Outlet /> : navigate("/");
 };
 
 export default ProtectedRoute;
